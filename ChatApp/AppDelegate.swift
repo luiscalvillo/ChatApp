11d2373a1d60138c23ff71167ac63e0f748045fd
Luis Calvillo <luiscalvillo@luiss-macbook-pro.local>
2019-04-06 12:27:56 -0700

    Created auto login function

--- ChatApp/AppDelegate.swift
+++ ChatApp/AppDelegate.swift
@@ -13,12 +13,32 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    var authListener: AuthStateDidChangeListenerHandle?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         FirebaseApp.configure()
+        
+        // Auto login
+        
+        authListener = Auth.auth().addStateDidChangeListener({ (auth, user) in
+            Auth.auth().removeStateDidChangeListener(self.authListener!)
+            
+            if user != nil {
+                
+                if UserDefaults.standard.object(forKey: kCURRENTUSER) != nil {
+                    
+                    DispatchQueue.main.async {
+                        self.goToApp()
+                    }
+                    
+                }
+            }
+        })
+        
         return true
     }
 
@@ -43,6 +63,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    // MARK: GoToApp
+    
+    func goToApp() {
+        
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: USER_DID_LOGIN_NOTIFICATION), object: nil, userInfo: [kUSERID : FUser.currentId()])
+      
+        let mainView = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "mainApplication") as! UITabBarController
+        
+        self.window?.rootViewController = mainView
+        
+    }
+    
 
 
 }
--- ChatApp/FinishRegistrationViewController.swift
+++ ChatApp/FinishRegistrationViewController.swift
@@ -116,6 +116,9 @@ class FinishRegistrationViewController: UIViewController {
     func goToApp() {
         cleanTextFields()
         dismissKeyboard()
+        
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: USER_DID_LOGIN_NOTIFICATION), object: nil, userInfo: [kUSERID : FUser.currentId()])
+        
         let mainView = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "mainApplication") as! UITabBarController
         
         self.present(mainView, animated: true, completion: nil)
